10d22266781f052a3867eac63de3d73583194cd5
GaahNoel <ga.noel@outlook.com>
2020-11-11 20:49:49 -0300

    fix team create

--- backend/src/team/team.module.ts
+++ backend/src/team/team.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/user/schema/user.schema';
 import { Team, TeamSchema } from './schema/team.schema';
 import { TeamController } from './team.controller';
 import { TeamService } from './team.service';
@@ -7,6 +8,7 @@ import { TeamService } from './team.service';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Team.name, schema: TeamSchema }]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
 
   controllers: [TeamController],
