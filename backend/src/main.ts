fb2eade3739d931a9ad728d910aee783414df90d
GaahNoel <ga.noel@outlook.com>
2020-11-20 19:43:32 -0300

    Settings for deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3333);
+  await app.listen(process.env.node_env === 'production' ? 80 : 3333);
 }
 bootstrap();
